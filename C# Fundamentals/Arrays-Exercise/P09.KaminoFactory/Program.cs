0ab5da171d4b716eb2dcbe7c2ca04d38c3bb9fa3
jordiboy <i_jordanov@yahoo.com>
2022-10-04 04:27:04 +0300

    Updated

---
+++ C# Fundamentals/Arrays-Exercise/P09.KaminoFactory/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+
+namespace P09.KaminoFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+
+            int samples = 0;
+            int bestSample = 0;
+            int bestCount = 0;
+            int bestSum = int.MinValue;
+            int smalerIndex = -1;
+            int[] bestDNASample = new int[length];            
+
+            string command = Console.ReadLine();
+
+            while (command != "Clone them!")
+            {
+                int[] dna = command    
+                    .Split("!", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                samples++;
+
+                int currentCount = 0;                
+                int counter = 0;
+                int sum = 0;
+                int currentIndex = -1;                
+                bool isBestDNA = false;
+
+                for (int i = 0; i < dna.Length; i++)
+                {
+                    if (dna[i] ==  0)
+                    {
+                        counter = 0;
+                        continue;
+                    }
+                    counter++;
+
+                    if (counter > currentCount)
+                    {
+                        currentCount = counter;                        
+                        currentIndex = i - currentCount + 1;
+                    }
+                }
+                                
+                sum = dna.Sum();
+
+                if (currentCount > bestCount)
+                {
+                    isBestDNA = true;
+                }
+                else if (currentCount == bestCount)
+                {
+                    if (currentIndex < smalerIndex)
+                    {
+                        isBestDNA = true;
+                    }
+                    else if (currentIndex == smalerIndex)
+                    {
+                        if (sum > bestSum)
+                        {
+                            isBestDNA = true;
+                        }
+                    }
+                }
+
+                if (isBestDNA)
+                {
+                    bestCount = currentCount;
+                    smalerIndex = currentIndex;
+                    bestSum = sum;
+                    bestSample = samples;
+                    bestDNASample = dna;                    
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Best DNA sample {bestSample} with sum: {bestSum}.");
+            Console.Write(string.Join(" ", bestDNASample));
+        }
+    }
+}
---
+++ C# Fundamentals/Arrays-Exercise/P10.LadyBugs/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+
+namespace P10.LadyBugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+
+            int[] field = new int[fieldSize];
+            int[] bugs = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            for (int i = 0; i < bugs.Length; i++)
+            {
+                if (bugs[i] > -1 && bugs[i] < field.Length)
+                {
+                    field[bugs[i]] = 1;
+                }                
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                string[] command = new string[3];
+
+                if (input != "end")
+                {
+                    command = input.Split().ToArray();
+                }
+                else
+                {
+                    break;
+                }
+
+                int startIndex = int.Parse(command[0]);
+                int flyLength = int.Parse(command[2]);
+
+                if (startIndex > field.Length - 1 || startIndex < 0 || field[startIndex] == 0)
+                {                    
+                    continue;
+                }
+                if (flyLength < 0 && command[1] == "right")
+                {
+                    command[1] = "left";
+                    flyLength = Math.Abs(flyLength);
+                }
+                else if (flyLength < 0 && command[1] == "left")
+                {
+                    command[1] = "right";
+                    flyLength = Math.Abs(flyLength);
+                }
+
+                field[startIndex] = 0;
+
+                if (command[1] == "right")
+                {                    
+
+                    while (startIndex + flyLength < field.Length)
+                    {
+                        if (field[startIndex + flyLength] == 1)
+                        {
+                            flyLength += flyLength;
+                        }
+                        else
+                        {
+                            break;
+                        }                        
+                    }
+                    if (startIndex + flyLength < field.Length)
+                    {                        
+                        field[startIndex + flyLength] = 1;                        
+                    }                    
+                }
+                else if (command[1] == "left")
+                {
+                    while (startIndex - flyLength > -1)
+                    {
+                        if (field[startIndex - flyLength] == 1)
+                        {
+                            flyLength += flyLength;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    if (startIndex - flyLength > -1)
+                    {                        
+                        field[startIndex - flyLength] = 1;                        
+                    }                    
+                }
+            }
+            Console.WriteLine(string.Join(" ", field));
+        }
+    }
+}

a4f66fa979e503150f1c1b77e5e0570f6b111248
jordiboy <i_jordanov@yahoo.com>
2025-08-30 22:03:18 +0300

    Updated, Problem 3

--- Entity Framework/05.Advanced-Querying-Exercises/BookShop/StartUp.cs
+++ Entity Framework/05.Advanced-Querying-Exercises/BookShop/StartUp.cs
@@ -12,8 +12,8 @@
             using var db = new BookShopContext();
             //DbInitializer.ResetDatabase(db);
 
-            string command = Console.ReadLine();
-            Console.WriteLine(GetBooksByAgeRestriction(db, command));
+            //string command = Console.ReadLine();
+            Console.WriteLine(GetGoldenBooks(db));
 
 
             //Problem 2 - Age Restriction
@@ -41,6 +41,29 @@
                 {
                     result
                         .AppendLine($"{bookTitle}");
+                }
+
+                return result.ToString().TrimEnd();
+            }
+
+            // Problem 3 - Golden Books
+
+            static string GetGoldenBooks(BookShopContext context)
+            {
+                StringBuilder result = new StringBuilder();
+
+                bool isEnumValid = Enum
+                    .TryParse("Gold", true, out EditionType editionType);                
+
+                string[] goldBooks = context.Books
+                    .Where(gb => gb.EditionType == editionType && gb.Copies < 5000)
+                    .Select(gb => gb.Title)
+                    .ToArray();
+                    
+                foreach (var goldBook in goldBooks)
+                {
+                    result
+                        .AppendLine(goldBook);
                 }
 
                 return result.ToString().TrimEnd();

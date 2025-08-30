c84503b601b3871e051b0c476748ca8bc8fdb735
jordiboy <i_jordanov@yahoo.com>
2025-08-30 16:36:03 +0300

    Initial Upload

---
+++ Entity Framework/05.Advanced-Querying-Exercises/BookShop/StartUp.cs
@@ -0,0 +1,52 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            string command = Console.ReadLine();
+            Console.WriteLine(GetBooksByAgeRestriction(db, command));
+
+
+            //Problem 2 - Age Restriction
+
+            static string GetBooksByAgeRestriction(BookShopContext context, string command)
+            {
+                StringBuilder result = new StringBuilder();
+
+                bool isEnumValid = Enum
+                    .TryParse(command, true, out AgeRestriction ageRestriction);
+
+                if (!isEnumValid)
+                {
+                    return string.Empty;
+                }
+
+                string[] bookTitles = context
+                    .Books
+                    .Where(b => b.AgeRestriction == ageRestriction)
+                    .OrderBy(b => b.Title)
+                    .Select(b => b.Title)
+                    .ToArray();
+
+                foreach (var bookTitle in bookTitles)
+                {
+                    result
+                        .AppendLine($"{bookTitle}");
+                }
+
+                return result.ToString().TrimEnd();
+            }
+        }
+    }
+}
+
+
